67f3923df02f8f6a9cc55b9ec46a3550a7b3185f
ikonovalov <konovalov84@gmail.com>
2017-03-07 00:30:18 +0300

    RB client creation test

--- src/test/java/ru/codeunited/ipfs/ClientTest.java
+++ src/test/java/ru/codeunited/ipfs/ClientTest.java
@@ -4,15 +4,18 @@ import com.netflix.ribbon.ClientOptions;
 import org.junit.Test;
 import ru.codeunited.ipfs.rb.ClientFactoryRb;
 
+import static com.google.common.truth.Truth.assertThat;
+
 /**
  * OSS codeunited.ru
  * Created by ikonovalov on 06/03/17.
  */
-class ClientTest {
+public class ClientTest {
 
     @Test
     public void createLocalClientOnRibbon() {
         ClientFactory<ClientOptions> clientFactory = ClientFactoryRb.newInstance();
-
+        Client client = clientFactory.createLocal();
+        assertThat(client).isNotNull();
     }
 }
